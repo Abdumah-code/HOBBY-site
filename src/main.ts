b86502aece5315c34f973dc41403b82729d13f27
abdu <abdumah2021fr@gmail.com>
2023-02-24 08:25:20 +0100

    add code to change bgc to clue when button is clicked

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.css'
+
+const styleBtn: HTMLElement = document.getElementById("styleChangeBtn")!;
+
+styleBtn.addEventListener("click", changeEverything);
+
+function changeEverything(): void{
+  document.body.style.backgroundColor = "blue";
+}
