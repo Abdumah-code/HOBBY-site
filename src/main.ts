f49edea2053429304a0b2165e9900b90aba43a3d
abdu <abdumah2021fr@gmail.com>
2023-02-26 19:42:34 +0100

    added more code to change text and display stuff when needed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './style.css'
 
 const styleBtn: HTMLElement = document.getElementById("styleChangeBtn")!;
 const bodyElement: any = document.getElementsByTagName("body")[0];
+const headerElement: any = document.getElementById("header");
+const footerElement: any = document.getElementById("footer");
+
 let isStyleSet1: boolean = true;
 const bodyStyle = bodyElement.style;
 
@@ -11,10 +14,16 @@ function toggleStyle(): void {
   if (isStyleSet1) {
     bodyStyle.backgroundImage = "url('./src/images/manga-bg.jpg')";
     bodyStyle.color = "black";
+    headerElement.textContent = "This is what manag looks like";
+    footerElement.textContent = "";
+    styleBtn.textContent = "Switch";
     isStyleSet1 = false;
   } else {
     bodyStyle.backgroundImage = "url('./src/images/anime-bg.jpg')";
     bodyStyle.color = "white";
+    footerElement.textContent = "This is what anime looks like";
+    headerElement.textContent = "";
+    styleBtn.textContent = "Switch";
     isStyleSet1 = true;
   }
 }
