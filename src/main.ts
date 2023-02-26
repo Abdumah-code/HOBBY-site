5427a6f62bbe97ea4678d43144c0a7fb1e2398eb
abdu <abdumah2021fr@gmail.com>
2023-02-26 21:09:02 +0100

    rewrote the function to have better readabelity

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,37 @@
 import './style.css'
 
 const styleBtn: HTMLElement = document.getElementById("styleChangeBtn")!;
-const bodyElement: any = document.getElementsByTagName("body")[0];
-const headerElement: any = document.getElementById("header");
-const footerElement: any = document.getElementById("footer");
+const bodyElement: HTMLElement = document.getElementsByTagName("body")[0];
+const headerElement: HTMLElement = document.getElementsByTagName("header")[0];
+const footerElement: HTMLElement = document.getElementsByTagName("footer")[0];
 
 let isStyleSet1: boolean = true;
 const bodyStyle = bodyElement.style;
 
 styleBtn.addEventListener("click", toggleStyle);
 
+function setHeaderText(headerText: string): void {
+  headerElement.textContent = headerText;
+  footerElement.textContent = "";
+}
+
+function setFooterText(footerText: string): void {
+  footerElement.textContent = footerText;
+  headerElement.textContent = "";
+}
+
 function toggleStyle(): void {
   if (isStyleSet1) {
     bodyStyle.backgroundImage = "url('./src/images/manga-bg.jpg')";
     bodyStyle.color = "black";
-    headerElement.textContent = "This is what manag looks like";
-    footerElement.textContent = "";
+    setHeaderText("This is what manga looks like (My fav hobby)");
     styleBtn.textContent = "Switch";
-    isStyleSet1 = false;
   } else {
     bodyStyle.backgroundImage = "url('./src/images/anime-bg.jpg')";
     bodyStyle.color = "white";
-    footerElement.textContent = "This is what anime looks like";
-    headerElement.textContent = "";
+    setFooterText("This is what anime looks like (My second fav)");
     styleBtn.textContent = "Switch";
-    isStyleSet1 = true;
   }
+  isStyleSet1 = !isStyleSet1;
 }
 
