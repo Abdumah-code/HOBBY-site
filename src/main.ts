36f87258047fe84e1d46521d75e41078ad0043ec
abdu <abdumah2021fr@gmail.com>
2023-02-26 18:39:01 +0100

    simplyfied the code a liitle bit

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import './style.css'
 const styleBtn: HTMLElement = document.getElementById("styleChangeBtn")!;
 const bodyElement: any = document.getElementsByTagName("body")[0];
 let isStyleSet1: boolean = true;
+const bodyStyle = bodyElement.style;
 
 styleBtn.addEventListener("click", toggleStyle);
 
 function toggleStyle(): void {
   if (isStyleSet1) {
-    bodyElement.style.backgroundColor = "white";
-    bodyElement.style.color = "black";
+    bodyStyle.backgroundColor = "white";
+    bodyStyle.color = "black";
     isStyleSet1 = false;
   } else {
-    bodyElement.style.backgroundColor = "black";
-    bodyElement.style.color = "white";
+    bodyStyle.backgroundColor = "black";
+    bodyStyle.color = "white";
     isStyleSet1 = true;
   }
 }
