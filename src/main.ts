84afc7e9e93d5c4f043a2bb77dcc7db8ff2134d3
abdu <abdumah2021fr@gmail.com>
2023-02-26 18:35:36 +0100

    done with functionality of changing the style of the page based on the button click

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import './style.css'
 
 const styleBtn: HTMLElement = document.getElementById("styleChangeBtn")!;
+const bodyElement: any = document.getElementsByTagName("body")[0];
+let isStyleSet1: boolean = true;
 
-styleBtn.addEventListener("click", changeEverything);
+styleBtn.addEventListener("click", toggleStyle);
 
-function changeEverything(): void{
-  document.body.style.backgroundColor = "blue";
+function toggleStyle(): void {
+  if (isStyleSet1) {
+    bodyElement.style.backgroundColor = "white";
+    bodyElement.style.color = "black";
+    isStyleSet1 = false;
+  } else {
+    bodyElement.style.backgroundColor = "black";
+    bodyElement.style.color = "white";
+    isStyleSet1 = true;
+  }
 }
+
