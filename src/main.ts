5ddc69566e1e2f4876c4029199894b319fbe1872
abdu <abdumah2021fr@gmail.com>
2023-02-26 19:28:43 +0100

    added background images and a styling black vail to be easy on the eyes

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ styleBtn.addEventListener("click", toggleStyle);
 
 function toggleStyle(): void {
   if (isStyleSet1) {
-    bodyStyle.backgroundColor = "white";
+    bodyStyle.backgroundImage = "url('./src/images/manga-bg.jpg')";
     bodyStyle.color = "black";
     isStyleSet1 = false;
   } else {
-    bodyStyle.backgroundColor = "black";
+    bodyStyle.backgroundImage = "url('./src/images/anime-bg.jpg')";
     bodyStyle.color = "white";
     isStyleSet1 = true;
   }
